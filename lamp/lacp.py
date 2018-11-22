4d701f4329df10ec2d5bfef2efad1a4a123e476a
wqh <634612447@qq.com>
2018-11-22 13:19:47 +0800

    first commit

---
+++ lamp/lacp.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Controller, RemoteController, OVSController
+from mininet.node import CPULimitedHost, Host, Node
+from mininet.node import OVSKernelSwitch, UserSwitch
+from mininet.node import IVSSwitch
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.link import TCLink, Intf
+from subprocess import call
+
+
+class MyTopo(Topo):
+    def __init__(self):
+        Topo.__init__(self)
+        net = Mininet( topo=None,
+                       build=False,
+                       ipBase='10.0.0.0/8')
+
+        info( '*** Adding controller\n' )
+        c0=net.addController(name='c0',
+                          controller=RemoteController,
+                          ip='127.0.0.1',
+                          protocol='tcp',
+                          port=6633)
+
+        info( '*** Add switches\n')
+        s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
+        info( '*** Add hosts\n')
+        h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
+        h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None, mac='00:00:00:00:00:22')        
+        h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute=None, mac='00:00:00:00:00:23')
+        h4 = net.addHost('h4', cls=Host, ip='10.0.0.4', defaultRoute=None, mac='00:00:00:00:00:24')
+
+        info( '*** Add links\n')
+        net.addLink(s1, h1)
+        net.addLink(s1, h1)        
+        net.addLink(s1, h2)
+        net.addLink(s1, h3)
+        net.addLink(s1, h4)
+
+
+        info( '*** Starting network\n')
+        net.build()
+        info( '*** Starting controllers\n')
+        for controller in net.controllers:
+            controller.start()
+
+        info( '*** Starting switches\n')
+        net.get('s1').start([c0])
+
+        info( '*** Post configure switches and hosts\n')
+        CLI(net)
+        net.stop()
+
+topos = {'mytopo':(lambda:MyTopo())}
+
